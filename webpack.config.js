242e8c673752b4a3f12ddcd254971e9eb732071a
Taylor Moore <Tmoore2272@gmail.com>
2016-11-01 10:04:06 -0600

    Adds most functionality

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,14 @@ module.exports = {
   },
   module: {
     loaders: [
-      { test: /\.js$/, exclude: '/node_modules/', loader: 'babel-loader' },
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+        presets: ['es2015', 'react'],
+        }
+      },
       { test: /\.css$/, loader: "style!css" },
       { test: /\.scss$/, loader: "style!css!sass" }
     ]
